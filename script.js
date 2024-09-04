8749751120233058b09e03758c9387908549a410
Kassandra Flores <kassyflores2013@gmail.com>
2024-09-04 11:43:06 -0500

    Created fortune teller webpage

---
+++ script.js
@@ -0,0 +1,21 @@
+const fortunes = [
+    "Luck is the residue of design.",
+    "Skill comes from diligence.",
+    "Pleasures await you by the sea.",
+    "Ignore previous fortunes.",
+    "You create your own stage and you audience is waiting.",
+    "Laugh long, loud, and often.",
+    "The stat of riches is shining on you",
+    "If you don't like the fortune don't eat the cookie.",
+    "You will be hungry again in 10 minutes."
+];
+
+
+document.getElementById('fortuneButton').addEventListener('click', function() {
+   
+const randomIndex = Math.floor(Math.random() * fortunes.length);
+const selectedFortune = fortunes[randomIndex];
+
+
+document.getElementById('fortune').textContent = selectedFortune;
+});
